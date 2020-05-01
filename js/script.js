8798e7462e4650535eab313fdeccf3413f1623a5
Tomppa-FI <TomppaFI@protonmail.com>
2020-05-01 14:08:40 +0300

    Locked positions to axis, added ability to organize bookmarks

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
+//Ensure DOM is fully loaded.
+
 window.addEventListener('DOMContentLoaded', (event) => {
-    let item;
-    let x;
-    let y;
 
-    document.addEventListener('mousedown', (event) => {
-      item = event.target.closest('.item');
-      x = event.clientX;
-      y = event.clientY;
+    const handleDragEnd = (event) => {
+        const clientY = event.clientY;
+        const clientX = event.clientX;
+        let lowestFlexOrder;
+
+        bookmarks.forEach(bookmark => {
+            if (bookmark === event.target) return;
+            
+            //Determine which coordinate to check against depending on flex-direction.
+
+            const {x, y} = bookmark.getBoundingClientRect();
+            const [clientCoordinate, bookmarkCoordinate] = window.matchMedia("(min-width: 768px)").matches ? [clientX, x] : [clientY, y];
+
+            //Set new flex-order if client coordinate is before the current bookmark.
+            if (clientCoordinate < bookmarkCoordinate) {
+                if (typeof lowestFlexOrder === "undefined" || lowestFlexOrder > parseInt(bookmark.style.order)) {
+                    lowestFlexOrder = parseInt(bookmark.style.order);
+                    console.log(lowestFlexOrder);
+                }
+                bookmark.style.order = parseInt(bookmark.style.order) + 1;
+            }
+        })
+        event.target.style.order = lowestFlexOrder;
+    }
+    
+    const bookmarkNodes = document.querySelectorAll(".item"); 
+    const bookmarks = [...bookmarkNodes].map((elem, i) => {
+        elem.addEventListener("dragend", handleDragEnd);
+        elem.style.order = i;
+        return elem;
     });
 
-    document.addEventListener('mousemove', (event) => {
-      if (!item) return;
-      item.style.transform = `translate(${event.clientX - x}px, ${event.clientY - y}px)`;
-    });
 
-    document.addEventListener('mouseup', (event) => {
-      item = null;
-    });
-})
+
+    //Select all bookmarks, deconstruct and iterate with map to apply flex-order.
+
+})    
