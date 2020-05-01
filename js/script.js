4d75974b71fca0517afdc109c5cc616a156d48f4
Tomppa-FI <TomppaFI@protonmail.com>
2020-05-01 15:08:24 +0300

    Slight adjustment to JS to allow for easier user interaction, formatting & styling

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
             if (bookmark === event.target) return;
             
             //Determine which coordinate to check against depending on flex-direction.
-
-            const {x, y} = bookmark.getBoundingClientRect();
-            const [clientCoordinate, bookmarkCoordinate] = window.matchMedia("(min-width: 768px)").matches ? [clientX, x] : [clientY, y];
+            //Additionally, slightly increase X/Y of bookmark, so can be repositioned easier.
+            const {x, y, width, height} = bookmark.getBoundingClientRect();
+            const [clientCoordinate, bookmarkCoordinate] = window.matchMedia("(min-width: 768px)").matches ? [clientX, (x + (width/3))] : [clientY, (y + (height / 3))];
 
             //Set new flex-order if client coordinate is before the current bookmark.
             if (clientCoordinate < bookmarkCoordinate) {
@@ -22,10 +22,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     console.log(lowestFlexOrder);
                 }
                 bookmark.style.order = parseInt(bookmark.style.order) + 1;
+                //Allow for forward movement.
+            } else if (clientCoordinate > bookmarkCoordinate) {
+                if (typeof lowestFlexOrder === "undefined" || lowestFlexOrder < parseInt(bookmark.style.order)) {
+                    lowestFlexOrder = parseInt(bookmark.style.order) + 1;
+                }
             }
         })
         event.target.style.order = lowestFlexOrder;
     }
+
+    //Select all bookmarks, deconstruct and iterate with map to apply flex-order.
     
     const bookmarkNodes = document.querySelectorAll(".item"); 
     const bookmarks = [...bookmarkNodes].map((elem, i) => {
@@ -33,9 +40,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
         elem.style.order = i;
         return elem;
     });
-
-
-
-    //Select all bookmarks, deconstruct and iterate with map to apply flex-order.
 
 })    
